f2b8678efa07f1305ffb5d1d5b814298444dcfd5
triline <dilipkmr911@gmail.com>
2017-04-13 11:47:02 +0530

    this is good

---
+++ app/src/main/java/com/example/triline/myapp/MainActivity.java
@@ -0,0 +1,76 @@
+package com.example.triline.myapp;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.ContactsContract;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btn;
+    TextView textView;
+    private static final int REQUEST_CODE = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn = (Button) this.findViewById(R.id.sendButton);
+
+
+        btn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+
+       // Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+       // startActivityForResult(intent, 2);
+
+
+        Uri uri = Uri.parse("content://contacts");
+        Intent intent = new Intent(Intent.ACTION_PICK, uri);
+        intent.setType(ContactsContract.CommonDataKinds.Phone.CONTENT_TYPE);
+        startActivityForResult(intent, REQUEST_CODE);
+
+    }
+
+
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+
+        super.onActivityResult(requestCode, resultCode, intent);
+        if (requestCode == REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                Uri uri = intent.getData();
+                String[] projection = { ContactsContract.CommonDataKinds.Phone.NUMBER,
+                        ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME };
+
+                Cursor cursor = getContentResolver().query(uri, projection,
+                        null, null, null);
+                cursor.moveToFirst();
+
+                int numberColumnIndex = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
+                String number = cursor.getString(numberColumnIndex);
+
+                int nameColumnIndex = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME);
+                String name = cursor.getString(nameColumnIndex);
+
+                Log.d("TAG", "ZZZ number : " + number +" , name : "+name);
+
+            }
+        }
+    };
+}
---
+++ app/src/main/java/com/example/triline/myapp/SecondActivity.java
@@ -0,0 +1,29 @@
+package com.example.triline.myapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class SecondActivity extends AppCompatActivity {
+
+    EditText et1;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+
+
+
+
+
+
+    }
+
+
+}
